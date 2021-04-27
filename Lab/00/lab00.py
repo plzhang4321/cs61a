766f43e5ddf327b3b5eabb03e14d887a2abcb4b1
plzhang <plzhang4321@126.com>
2021-04-27 23:49:34 +0800

    update

---
+++ Lab/00/lab00.py
@@ -0,0 +1,12 @@
+def twenty_twenty_one():
+    """Come up with the most creative expression that evaluates to 2021,
+    using only numbers and the +, *, and - operators.
+
+    # >>> twenty_twenty_one()
+    2021
+    """
+    return print(2000 + 21)
+
+
+if __name__ == "__main__":
+    twenty_twenty_one()
